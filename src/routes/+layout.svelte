f39dda26b4829138d7e962a8aca830825cd8c6f4
Massart <quentinmassart93@gmail.com>
2024-05-02 20:02:57 +0200

    added deps

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<h1 class="text-red-500 fl">Hello</h1>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	future: {
+		hoverOnlyWhenSupported: true
+	},
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
